6ffa235fdded9f9bb922bc3d47edf71f955b0a8c
lunerfox <lunerfox@gmail.com>
2016-07-06 14:41:54 -0700

    Translated the order page to chinese

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 
-app = angular.module('storeApp', []); 
+app = angular.module('storeApp', []);
 
-app.controller('orderController', [              
-    '$scope',                              
-    function orderController($scope) {  
+app.controller('orderController', [
+    '$scope',
+    function orderController($scope) {
 
 
   $scope.options = [
@@ -14,8 +14,8 @@ app.controller('orderController', [
     { label: '4', value: 4 },
     { label: '5', value: 5 }
   ];
- 
- 
+
+
   $scope.myBed = $scope.options[0];
   $scope.myNightstand = $scope.options[0];
   $scope.myHammock = $scope.options[0];
@@ -25,13 +25,13 @@ app.controller('orderController', [
 ]);
 
 
-app.controller('productsController', [              
-    '$scope',                              
-    function productsController($scope) {        
+app.controller('productsController', [
+    '$scope',
+    function productsController($scope) {
         $scope.items = [
-      {name: 'Bed', price: '699', oldprice: '760'},
-      {name: 'Nightstand', price: '399', oldprice: '450'},
-      {name: 'Hammock', price: '599', oldprice: '640'}
-       ];                 
-    }                                                
-]); 
+      {name: '罐裝蠔皇鮑魚 - 五顆', price: '$65', oldprice: '$70'},
+      {name: '罐裝蠔皇鮑魚 - 六顆', price: '$68', oldprice: '$73'},
+      {name: '罐裝蠔皇鮑魚 - 七顆', price: '$70', oldprice: '$75'}
+       ];
+    }
+]);
